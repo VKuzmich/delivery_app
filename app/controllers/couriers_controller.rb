3e49a54607af62a9c80fd0c4b4ae79e316839951
Vladimir Kuzmich <vjk1976@ukr.net>
2020-03-06 01:23:48 +0200

    creating Courier model and controller and views

---
+++ app/controllers/couriers_controller.rb
@@ -0,0 +1,52 @@
+class CouriersController < ApplicationController
+  before_action :set_courier, only: %i[show edit update destroy]
+
+  def index
+    @couriers = Courier.all.order('created_at DESC')
+  end
+
+  def show
+  end
+
+  def new
+    @courier = Courier.new
+  end
+
+  def create
+    @courier = Courier.new(courier_params)
+
+    if @courier.save
+      redirect_to couriers_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+
+    if @courier.update(params[:courier].permit(:name, :email))
+      redirect_to @courier
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @courier.destroy
+
+    redirect_to couriers_path
+  end
+
+  private
+
+  def set_courier
+    @courier = Courier.find(params[:id])
+  end
+
+  def courier_params
+    params.require(:courier).permit(:name, :email)
+  end
+end
