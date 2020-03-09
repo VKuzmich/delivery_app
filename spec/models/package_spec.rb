e8a7e851821f15ee5dcd4fa5d458cad25c463b3f
Vladimir Kuzmich <vjk1976@ukr.net>
2020-03-09 22:56:16 +0200

    rspec tests on package model

--- spec/models/package_spec.rb
+++ spec/models/package_spec.rb
@@ -3,5 +3,8 @@
 require 'rails_helper'
 
 RSpec.describe Package, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should belong_to(:courier) }
+  it { should validate_presence_of(:tracking_number) }
+  it { should validate_length_of(:tracking_number).is_at_least(10).is_at_most(20)}
+  # it { should validate_inclusion_of(:delivery_status).in_array([true, false])}
 end
